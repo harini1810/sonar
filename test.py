b9f80654de97461fa1ceec3bd3417f60d9c02cb0
harini1810 <137413662+harini1810@users.noreply.github.com>
2025-01-05 19:00:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("hello")
+def add(a, b):
+    """Function to add two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Function to subtract two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    print("Addition: ", add(5, 3))
+    print("Subtraction: ", subtract(5, 3))
+
